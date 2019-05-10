a6bec03051a0c766968088c5e7c0a3f0bb0ec50c
shaansubbaiah <shaansubbaiah@yahoo.com>
2019-05-10 18:53:23 +0530

    Fixed output table structure, styling.

--- main.c
+++ main.c
@@ -16,9 +16,10 @@ void Display();
 
 int i, j, k, choice=1;
 
+// Structure to store student details
 struct student{
     int sem;
-    char USN[10];
+    char USN[11];
     char name[20];
     int subMarks[5];
     int subGrade[5];
@@ -26,6 +27,7 @@ struct student{
 };
 struct student stu[10];
 
+// Structure to store subject details
 struct subject{
     char name[20];
     int credits;
@@ -33,7 +35,13 @@ struct subject{
 struct subject sub[5];
 
 int main() {
-    printf("~~~~~~~~ Application to calculate SGPA ~~~~~~~~\n");
+    // Greeting
+    printf("~~~~~~~~ Application To Calculate SGPA ~~~~~~~~\n");
+    printf("~                                             ~\n");
+    printf("~                                             ~\n");
+    printf("~                                             ~\n");
+    printf("~~~~~~~~~~~~~~~~~~~ Welcome ~~~~~~~~~~~~~~~~~~~\n");
+
 
     // Input Subject Details
     getSubjectDetails();
@@ -42,38 +50,44 @@ int main() {
     while(choice==1){
         // Input Student Details, Marks, etc
         getStudentDetails();
+
         // Convert Marks to Grade Equivalent
         marksToGrade();
+
         // Calculate SGPA from marks inputted
         calSGPA();
+
         // Ask to continue
-        printf("Press 1 to enter details for another student.\nPress any other key to view table and exit.\n");
+        printf("\nEnter 1 to enter details for another student.\n \t OR \nEnter any other key to view table and exit.\n");
         scanf("%d", &choice);
+
+        // Accepts input for another student if choice=1, else displays table and quits program.
         if(choice!=1){
             Display();
-            printf("\n\nExiting.\n");
+            printf("\n\n~~~~~~~~~~~~~~~~~~~ Exiting. ~~~~~~~~~~~~~~~~~~\n");
             exit(1);
         }
         else
-            i++; // To add another student, we increment i as all the functions work with stu[i]
+            // To add another student, we increment i as all the functions work with stu[i]
+            i++;
     }
     return 0;
 }
 void getSubjectDetails(){
-    printf("Enter the name and credits of the subject:\n(eg. \"MAT 4\") Note: Total credits should equal 20.\n");
+    printf("Enter the name and credits of each subject:\n(ex. 'MATH 4'.)\n");
     for(i=0;i<5;i++){
-        scanf("%s %d",sub[i].name,&sub[i].credits);
+        scanf("%s %d", sub[i].name, &sub[i].credits);
     }
 }
 void getStudentDetails(){
-    printf("Enter student details:\n");
+    printf("\nEnter student details:\n");
     printf("Sem - ");
-    scanf("%d",&stu[i].sem);
+    scanf("%d", &stu[i].sem);
     printf("USN - ");
-    scanf("%s",stu[i].USN);
+    scanf("%s", stu[i].USN);
     printf("Name - ");
-    scanf("%s",stu[i].name);
-    printf("Enter marks in %s %s %s %s %s:\n(eg. \"90 32 43 54 65\")\n",sub[0].name, sub[1].name, sub[2].name, sub[3].name, sub[4].name);
+    scanf("%s", stu[i].name);
+    printf("Enter marks in \n %s %s %s %s %s:\n(eg. \"90 32 43 54 65\")\n",sub[0].name, sub[1].name, sub[2].name, sub[3].name, sub[4].name);
     scanf("%d %d %d %d %d", &stu[i].subMarks[0], &stu[i].subMarks[1], &stu[i].subMarks[2], &stu[i].subMarks[3], &stu[i].subMarks[4]);
 }
 void marksToGrade(){
@@ -100,8 +114,11 @@ void calSGPA(){
 
 }
 void Display(){
-    printf("Sl.No \t Sem \t USN \t Name \t %s \t %s \t %s \t %s \t %s \t SGPA \n", sub[0].name, sub[1].name, sub[2].name, sub[3].name, sub[4].name);
+    // Prints title row of table
+    printf("\n%3s %3s \t %10s %15s %5s %5s %5s %5s %5s %5s \n", "SL", "SEM", "USN", "NAME", sub[0].name, sub[1].name, sub[2].name, sub[3].name, sub[4].name, "SGPA");
+
+    // Prints individual student data on each row
     for (j=0; j<=i;j++){
-        printf("%d \t\t\t %d \t %s \t %s \t %d \t %d \t %d \t %d \t %d \t %d \n\n", j, stu[j].sem, stu[j].USN, stu[j].name, stu[j].subMarks[0], stu[j].subMarks[1], stu[j].subMarks[2], stu[j].subMarks[3], stu[j].subMarks[4], stu[j].SGPA);
+        printf("%3d %3d \t %10s %15s %5d %5d %5d %5d %5d %5d \n\n", j+1, stu[j].sem, stu[j].USN, stu[j].name, stu[j].subMarks[0], stu[j].subMarks[1], stu[j].subMarks[2], stu[j].subMarks[3], stu[j].subMarks[4], stu[j].SGPA);
     }
 }
